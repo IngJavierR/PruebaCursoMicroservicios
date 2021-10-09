2c8341ee495454cf79bd9a9df3d8a1b52c33c703
Javier Rodriguez <francisco.rodriguez@axity.com>
2021-10-08 22:59:24 -0500

    Microservicios-feing

--- microservicio-service/microservicio-services/src/main/java/mx/com/curso/services/facade/impl/MicroservicioFacade.java
+++ microservicio-service/microservicio-services/src/main/java/mx/com/curso/services/facade/impl/MicroservicioFacade.java
@@ -2,6 +2,7 @@ package mx.com.curso.services.facade.impl;
 
 import mx.com.curso.commons.to.UserTO;
 import mx.com.curso.services.facade.IMicroservicioFacade;
+import mx.com.curso.services.feign.ServiceTwoClient;
 import mx.com.curso.services.service.IMicroservicioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,7 +14,11 @@ public class MicroservicioFacade implements IMicroservicioFacade {
     @Autowired
     private IMicroservicioService microservicioService;
 
+    @Autowired
+    private ServiceTwoClient serviceTwoClient;
+
     public List<UserTO> getAllUsers() {
-        return this.microservicioService.getUsers();
+        return this.serviceTwoClient.getUsers();
+        //return this.microservicioService.getUsers();
     }
 }
