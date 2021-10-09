8c916efa9965a910e9fffe98d6eaf741c876845a
Javier Rodriguez <francisco.rodriguez@axity.com>
2021-10-08 22:03:53 -0500

    Microservicios

--- gatling/src/test/scala/computerdatabase/BasicSimulation.scala
+++ gatling/src/test/scala/computerdatabase/BasicSimulation.scala
@@ -7,7 +7,7 @@ import scala.concurrent.duration._
 class BasicSimulation extends Simulation {
 
   val httpProtocol = http
-    .baseUrl("http://192.168.1.133:8090/microservicio") // Here is the root for all relative URLs
+    .baseUrl("http://192.168.1.133:8000/apimicros/microservicio") // Here is the root for all relative URLs
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-US,en;q=0.5")
